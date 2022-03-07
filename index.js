042358b59462e9f1376a83861a8690b45c46e56e
s <savannay@uw.edu>
2022-03-07 07:39:59 +0000

    factored out instructions

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const clear = require('./buttons/clear.js');
 const leave = require('./buttons/leave.js');
 const reopen = require('./buttons/reopen.js');
 const send = require('./buttons/send.js');
+const intros = require('./intros.js');
 
 
 const clientId = process.env.CLIENT_ID;
@@ -30,45 +31,17 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 });
 
-///////////instructions//////////////
-const help_string = 'Only the bot is able to post in this channel. The posts in this channel are anonymous requests for support. To send an anonymous request for support, use the `/request` command. Your request will appear here, where you can remove it. Only the person who made the request can remove it from this channel. Others can respond to a post by offering support. Offering support creates a private channel within the server between the person who requested support and the person who offered it. Once someone has offered support to a request, it will be moved from this channel to `#help-archive`. From there, the requester can remove the message or re-open the message, sending it back here where someone can offer help.';
-const archive_string = 'Only the bot is able to post in this channel. The posts in this channel are anonymous requests for support that have been moved to here from `#help` because they have been offered help already. To send an anonymous request for support, use the `/request` command. Your request will appear in `#help`. Only the person who made the request can remove it from this channel. Only the requester can re-open a message, sending it back to `#help` where someone can offer help again.';
-const request_string = 'Use this command to request help. Fill in `message` with any context on what you’d like support for. Use `urgent` to specify whether your request is urgent. Specifying an urgent request will ping everyone set to `available`. Fill in `looking-for` to specify whether you’re looking to vent, get advice, or just talk to someone. Fill in `support-type` to indicate whether you want to be supported only, would like to find mutual support, or are open to either. Once you fill in your request the bot will post on your behalf in `#help`, keeping you anonymous until someone offers to help you.';
-
-const start_intro = new MessageEmbed()
-  .setColor('#FDA50F')
-  .setTitle('Welcome to the capstone test server!')
-  .setDescription('The goal of this server is to test out the bot. The bot\'s job is to help facilitate 1:1 peer support for mental health. The main things to do here are 1) ask for support and 2) offer support.')	
-  .addFields(
-  { name: 'Using slash commands', value: 'To use slash commands, simply type \"/\" followed by the command. You can\'t type in this channel, but you can use these commands in `#commands`. The command you type won\'t be visible to anyone but you.' },
-  { name: 'Using `/available`', value: 'Turn notifications for when someone needs urgent help on or off. These are off by default.', inline: true },
-  { name: 'Using `/request`', value: request_string, inline: true },
-  { name: 'Using `#help`', value: help_string },
-  { name: 'Using `#help-archive`', value: archive_string },
-);
-
-const help_intro = new MessageEmbed()
-  .setColor('#FDA50F')
-  .setTitle('Using this channel')
-  .setDescription(help_string);	
-
-const archive_intro = new MessageEmbed()
-  .setColor('#FDA50F')
-  .setTitle('Using this channel')
-  .setDescription(archive_string);	
-
-client.on("messageCreate", msg => {
+client.on("messageCreate", msg => { //generate instructions
   if (msg.member.permissionsIn(msg.channel).has("ADMINISTRATOR")){
     if (msg.content === "!start") {
-      return msg.channel.send({ embeds: [start_intro], ephemeral: false });
+      return intros.start_intro(msg);
     } else if (msg.content === "!help") {
-      return msg.channel.send({ embeds: [help_intro], ephemeral: false });
+      return intros.help_intro(msg);
     } else if (msg.content === "!archive") {
-      return msg.channel.send({ embeds: [archive_intro], ephemeral: false });
+      return intros.archive_intro(msg);
     }
   }
 });
-////////////////end instructions//////////////////
 
 client.on('interactionCreate', async interaction => {
   if(interaction.isCommand()) {
